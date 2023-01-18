a49b060c461a8c40d53cbb7ca030dc1cbb162e73
Arthur Bernardo <arthur.bernardo@valoribank.com>
2023-01-18 18:05:46 -0300

    Comentários com explicações

--- valoridash/Server/index.js
+++ valoridash/Server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mysql = require('mysql');
 const cors = require('cors');
 
+// Conexão com o banco de dados
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
@@ -10,16 +11,33 @@ const db = mysql.createPool({
     database: "valoridashdb"
 });
 
+// Política de cors
 app.use(cors());
+// Usado para transformar a requisição para formato json.
+// Sem isso, a requisição é enviada para o servidor mas não conseguimos tirar as informações dela
 app.use(express.json());
 
+
+app.get('/', (req, res) => {
+    let sql = "SELECT * FROM users"
+    db.query(sql, (res, err) => {
+        console.log(res);
+    })
+})
+
+
+
+// Método post para criar um novo usuário
 app.post('/register', (req, res) => {
+    // A requisição veio como json, então desestruturamos seu corpo para pegar os valores necessários
     const { email } = req.body;
     const { password } = req.body;
     const { name } = req.body;
 
+    // comando sql para inserção de novos usuários na tabela
     let sql = "INSERT INTO users (email, password, name) VALUES (?, ?, ?)";
 
+    // consulta sql com os dados da requisição
     db.query(sql, [email, password, name], (err, res) => {
         console.log(err);
     })
