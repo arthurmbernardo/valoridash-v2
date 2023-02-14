698ddbdf3c68615124f1223fc7f421f0bd22097c
Arthur Bernardo <arthur.bernardo@valoribank.com>
2023-02-14 17:39:50 -0300

    Atualização do formulário de notícias

--- valoridash/Client/src/components/news/RegisterNews.js
+++ valoridash/Client/src/components/news/RegisterNews.js
@@ -47,7 +47,7 @@ function RegisterNews () {
                     <p>Nome:</p>
                     <input type="text" name="title" placeholder="Título" value={news.title} onChange={handleInput} required />
                     <p>Descrição:</p>
-                    <textarea rows="15" cols="40" name="descrip" value={news.descrip} onChange={handleInput}>Escreva o news aqui</textarea>
+                    <textarea required rows="15" cols="40" name="descrip" value={news.descrip} onChange={handleInput}>Escreva o news aqui</textarea>
                     <p>Data:</p>
                     <input type="date" name="dt_creation" placeholder="Data" value={news.dt_creation} onChange={handleInput} required />
                     <br /><br />
