c121d6c4623f7b41ae5d2f69e085ec6fbfae7a55
Arthur Bernardo <arthur.bernardo@valoribank.com>
2023-02-24 14:22:11 -0300

    Adição de novos relatórios

--- valoridash/Client/src/components/relatorios/Atendimento.js
+++ valoridash/Client/src/components/relatorios/Atendimento.js
@@ -14,6 +14,7 @@ function Atendimento() {
             </div>
 
             <div id="visibleIframeContainer">
+                <h1>PPV Atendimento</h1>
                 <iframe title="PPV ATENDIMENTO" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=1a85635c-b5ed-4196-ac46-800fa68d946b&autoAuth=true&ctid=0a42edab-afcd-48af-a0b9-5b78edb164da" frameBorder='0' allowFullScreen></iframe>
             </div>
         </div>
--- valoridash/Client/src/components/relatorios/Comercial.js
+++ valoridash/Client/src/components/relatorios/Comercial.js
@@ -9,6 +9,11 @@ function Comercial() {
             <Header />
             <div className="backButtonContainer">
                 <button class="backButton" onClick={() => { navigate('/relatorios') }}>Voltar</button>
+
+            </div>
+            <h1>RGV Comercial</h1>
+            <div id="visibleIframeContainer">
+                <iframe title="RGV_Comercial_V4" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=e12b831e-3501-458e-999c-e22dfb0c3f07&autoAuth=true&ctid=0a42edab-afcd-48af-a0b9-5b78edb164da" frameborder="0" allowFullScreen="true"></iframe>
             </div>
         </div>
     )
